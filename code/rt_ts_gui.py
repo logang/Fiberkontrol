6b25adbfc0f51beb7f3aabb387fcb3af542b3e11
Kelly Z <kellyz@dn0a20f90e.sunet>
2011-04-06 14:01:11 -0700

    gui mostly working

--- code/rt_ts_gui.py
+++ code/rt_ts_gui.py
@@ -1,15 +1,18 @@
 # Major imports
-from numpy import linspace, pi
 import numpy as np
 import serial
 import re
 import time
+import random
+import wx
 
 # Enthought imports
 from enthought.traits.api \
-    import Array, Dict, Enum, HasTraits, Str, Int, Range, Button, String
-from enthought.traits.ui.api import Item, View, HGroup, spring
-from enthought.traits.api import Instance
+    import Array, Dict, Enum, HasTraits, Str, Int, Range, Button, String, \
+    Bool, Callable, Float, Instance, Trait
+from enthought.traits.ui.api import Item, View, Group, HGroup, spring, Handler
+from enthought.pyface.timer.api import Timer
+
 
 # Chaco imports
 from enthought.chaco.api import ArrayPlotData, Plot
@@ -17,96 +20,64 @@ from enthought.enable.component_editor import ComponentEditor
 from enthought.chaco.chaco_plot_editor import ChacoPlotEditor, \
                                                 ChacoPlotItem
 
+from threading import Thread
+
 #------------------------------------------------------------------------------------------
 
-class FiberPlot( HasTraits ):
+class FiberModel( HasTraits ):
 
     # Public Traits
-    plot          = Instance( Plot )
     plot_type     = Enum( "record", "demo" )
     analog_in_pin = Int( 0 )
-    record        = Button()
-    stop          = Button()
-    load_data     = Button()
-    save_data     = Button()
-    save_plot     = Button()
-    T             = Range( 0, 1000000 ) # Do we need a stop time? Stop button instead?
+    T             = Int( 0 ) 
     dt            = Int( 20 ) # in ms; what does this do to accuracy
-    band_low      = Range( 0., 1024 )
-    band_high     = Range( 0., 1024 )
-    output        = Array
-    plotdata      = ArrayPlotData()
 
     # Private Traits
-    #_ydata  = Array
-    #_xdata  = Array
-
-    # Default TraitsUI view
-    traits_view = View(
-        Item('plot', editor=ComponentEditor(), show_label=False),
-        # Items
-        HGroup( spring,
-                Item( "record",    show_label = False ), spring,
-                Item( "stop",      show_label = False ), spring,
-                Item( "load_data", show_label = False ), spring,
-                Item( "save_data", show_label = False ), spring,
-                Item( "save_plot", show_label = False ), spring,
-                Item( "plot_type" ),                     spring,
-                Item( "analog_in_pin" ),                 spring,
-                Item( "dt" ),                            spring ),
-
-        Item( "T" ),
-        HGroup( Item( "band_high" ),
-                Item( "band_low" ) ),
-        # GUI window
-        resizable = True,
-        width     = 1000, 
-        height    = 700 ,
-        kind      = 'live' )
+    _ydata  = Array()
+    _xdata  = Array()
 
     def __init__(self, **kwtraits):
-        super( FiberPlot, self ).__init__( **kwtraits )
-
-#        self.plotdata = ArrayPlotData()
-        self.plotdata.set_data( "y", np.array(()) ) 
-        self.plotdata.set_data( "x", range( len( self.plotdata.get_data("y")  ) ) )
-
-        plot = Plot( self.plotdata )
-        plot.plot(("x", "y"), type="line", color="green")
-        self.plot = plot
-
-        self.band_low  = 0
-        self.band_high = 1024
-#        self.T          = 1000000
-
+        super( FiberModel, self ).__init__( **kwtraits )
+ 
         if self.plot_type is "record":
             self.buffer    = ''
             self.rate      = 115200
-            self.recording = False
+            self.recording = True
             try:
-                self.ser = serial.Serial( '/dev/tty.usbserial-A600eu7L', self.rate )
+                self.ser = serial.Serial( '/dev/tty.usbmodem641', self.rate )
                 print "serial initialized"
             except:
-                self.ser = serial.Serial( '/dev/tty.usbmodem411', self.rate )                
+                self.ser = serial.Serial( '/dev/tty.usbserial-A600emRx', self.rate )                
                 print "serial initialized"
 
-    @property
-    def listen( self ):
+#    def run( self ):
+#        for i in range(50):
+#            if self.recording is True:
+#                self._get_current_data()
+
+    def save( self, path = None, name = None ):
+        if path is not None:
+            self.savepath = path
+        if name is not None:
+            self.outname = name
+        np.savez( self.savepath + self.outname, self.output )
+
+    def load( self, path = None, name = None ):
+        if path is not None:
+            self.savepath = path
+        self.loaded = np.load( self.savepath + '/out.npz' )['arr_0']
+
+    def _get_current_data( self ):
 
         if self.recording is True:
-#            try:
             hnew = np.array( self.receiving() )
+
             #except:
             #    hnew = 0. #self._ydata[-1] 
             #    print "missed a serial read listening"
 
-        output  = np.append( self.plotdata.get_data("y"), hnew )
-        print self.plotdata.get_data("y"), hnew, output
-
-#        self.plotdata.set_data( "y",  output ) 
-#        self.plotdata.set_data( "x", range( len( self.plotdata.get_data("y")  ) ) )
-
-        return output
+            self._ydata = np.append( self._ydata, hnew )
+            self._xdata = range( len( self._ydata ) )
 
     def receiving( self, dt = None ):
 
@@ -132,7 +103,7 @@ class FiberPlot( HasTraits ):
                         out.append( int( o[0] ) )
 
             self.buffer = lines[1]
-
+            out = np.median( out[~(out == 0)] )
 #        except:
 #            print "missed a serial read receiving"
 
@@ -141,36 +112,65 @@ class FiberPlot( HasTraits ):
     def plot_out( self ):
         pl.plot( self.out )
 
-    def save( self, path = None, name = None ):
-        if path is not None:
-            self.savepath = path
-        if name is not None:
-            self.outname = name
-        np.savez( self.savepath + self.outname, self.output )
 
-    def load( self, path = None, name = None ):
-        if path is not None:
-            self.savepath = path
-        self.loaded = np.load( self.savepath + '/out.npz' )['arr_0']
+class FiberView( FiberModel ):
 
+    plot_data     = Instance( ArrayPlotData )
+    plot          = Instance( Plot )
+    record        = Button()
+    stop          = Button()
+    load_data     = Button()
+    save_data     = Button()
+    save_plot     = Button()
+
+    # Default TraitsUI view
+    traits_view = View(
+        Item('plot', editor=ComponentEditor(), show_label=False),
+        # Items
+        HGroup( spring,
+                Item( "record",    show_label = False ), spring,
+                Item( "stop",      show_label = False ), spring,
+                Item( "load_data", show_label = False ), spring,
+                Item( "save_data", show_label = False ), spring,
+                Item( "save_plot", show_label = False ), spring,
+                Item( "plot_type" ),                     spring,
+                Item( "analog_in_pin" ),                 spring,
+                Item( "dt" ),                            spring ),
+
+        Item( "T" ),
+        # GUI window
+        resizable = True,
+        width     = 1000, 
+        height    = 700 ,
+        kind      = 'live' )
+
+    def __init__(self, **kwtraits):
+        super( FiberView, self ).__init__( **kwtraits )
+
+        self.plot_data = ArrayPlotData( x = self._xdata, y = self._ydata )
+
+        self.plot = Plot( self.plot_data )
+        renderer  = self.plot.plot(("x", "y"), type="line", color="green")[0]
+
+    def run( self ):
+        for i in range( 100 ):
+            self._plot_update()
+            self._get_current_data()
+
+
+    def _plot_update( self ):
+
+        self.plot_data.set_data( "y", self._ydata ) 
+        self.plot_data.set_data( "x", self._xdata )
+        self.plot = Plot( self.plot_data )
+        self.plot.plot(("x", "y"), type="line", color="green")[0]
+        print 1
+        self.plot.request_redraw()
+
+    # Note: These should be moved to a proper handler
     def _record_fired( self ):
         self.recording = True
         self.run()
-
-    def run( self ):
-        counter = 0
-        self.T = 100
-        for i in range( self.T ):
-            if self.recording is True:
-                self.output = self.listen
-                print self.output
-#                self.plotdata.set_data( "y",  self.output ) 
-#                self.plotdata.set_data( "x", range( len( self.plotdata.get_data("y")  ) ) )
-                print self.plotdata.get_data("y")
-
-                counter += 1
-            else:
-                counter = 10
 
     def _stop_fired( self ):
         self.recording = False
@@ -179,35 +179,13 @@ class FiberPlot( HasTraits ):
         pass
 
     def _save_data_fired( self ):
-        S = Save( save = Save(), display=TextDisplay() )
+        S = Save( save = Save(), display = TextDisplay() )
         S.configure_traits()
         
         self.save( path = S.savepath, name = S.outname )
 
     def _save_plot_fired( self ):
         pass
-
-#    @property
-#    def _get_slice( self ):
-#        if self.plot_type is "demo":
-#            data_array = np.load( "../../../../../Data/20101119_fiberkontrol/gc_baseline_to_iso.npz" )
-#            return data_array['arr_0'][0:int(self.T)]
-#        else:
-#            pass
-
-#    def _output_changed( self ):
-#        self._ydata = self.output
-#        self._xdata = range( len( self._ydata ) )                
-
-#    def _counter_changed( self ):
-#        self.listen()
-#        self._ydata = self.output
-#        self._xdata = range( len( self._ydata ) )                
-
-#    def _T_changed( self ):
-#        self._ydata = self._get_slice
-#        self._xdata = range( len( self._ydata ) )        
-
 
 
 #------------------------------------------------------------------------------------------
@@ -245,7 +223,9 @@ class SaveDialog(HasTraits):
 
 
 if __name__ == "__main__":
-    F = FiberPlot()
+    F = FiberView()
     F.recording = True
-    F.listen
+#    F.run()
+#    1/0
+#    F._get_current_data()
     F.configure_traits()
