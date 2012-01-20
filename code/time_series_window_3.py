eb430cb7a979839f23e1298e47f9ac7c4b5114b9
izkula <ikauvar@stanford.edu>
2012-01-20 02:09:15 -0800

    Added frequency control, slow readin, save capability to labjack gui

--- code/time_series_window_3.py
+++ code/time_series_window_3.py
@@ -28,6 +28,7 @@ from threading import Thread
 #-----------------------------------------------------------------------------------------#
 
 class FiberModel( HasTraits ):
+    # Traits - other functions are listening for this to change
 
     # Public Traits
     plot_type     = Enum( "record", "demo" )
@@ -35,7 +36,7 @@ class FiberModel( HasTraits ):
     T             = Int( 0 ) 
     dt            = Int( 1 ) # in ms; what does this do to accuracy
     analog_in_0   = Int( 1 ) 
-    analog_in_1   = Int( 0 ) 
+    analog_in_1   = Int( 1 ) 
     analog_in_2   = Int( 0 ) 
     analog_in_3   = Int( 0 ) 
 
@@ -51,13 +52,11 @@ class FiberModel( HasTraits ):
 
     # Private Traits
     _ydata  = Array()
-    _xdata  = Array()
+    _i1data = Array() #input 1 data
+    _i2data = Array() #input 2 data
+    _i3data = Array() #input 3 data
     _tdata  = Array() #time data
     _sdata  = Array() #shutter data
-    
-
-    shutterPlan = [0, 1, 0, 3, 0, 5, 0, 7] #in Hz
-    
 
 
     def __init__(self, **kwtraits):
@@ -68,7 +67,7 @@ class FiberModel( HasTraits ):
 
  
         # debugging flag
-        self.debug = True
+        self.debug = False
 
         if self.plot_type is "record":
             try:
@@ -84,31 +83,102 @@ class FiberModel( HasTraits ):
 
             # setup analog channel registers
             self.AIN0_REGISTER = 0
+            self.AIN1_REGISTER = 2
+            self.AIN2_REGISTER = 4
+            self.AIN3_REGISTER = 6
+
             self.num_analog_channels = self.analog_in_0 + self.analog_in_1 + self.analog_in_2 + self.analog_in_3
+
+
+            # setup frequency control
+
+            #---Constants for PWM frequency-----
+            self.TIME_CLOCK_BASE = 4000000 #in Hz
+            self.FREQUENCY_CONVERSION_CONSTANT = 65536
+            self.PWM_CONSTANT = 65536
+            
+
+            self.labjack.writeRegister(6002, 1) #change FI02 to output
+            self.labjack.writeRegister(50501, 1) # enable one timer
+            self.labjack.writeRegister(50500, 2) #change the offset (output pin) to FIO2            
+
+            self.labjack.writeRegister(7100, 0) #for 16bit PWM output
+            self.labjack.writeRegister(7000, 4) #for 4 MHz/divisor base clock
+            self.labjack.writeRegister(7200, 65535) #init the  duty cycle to 0%
+
+
+            #---Shutter plan settings ------------
+            self.start_time = time.time()
+            self.shutterChunkTime = 5 #in seconds
+
+            self.shutterPlan = [10, 0, 3, 0, 5, 0, 7, 0] #in Hz
+
 
             # set recording to be true
             self.recording = True
+
+
+
 
     def _get_current_data( self ):
         """ If currently recording, query labjack for current analog input registers """
         if self.recording is True:
             current = self.labjack.readRegister( self.AIN0_REGISTER, numReg = self.num_analog_channels*2 )
-            self._ydata = np.append( self._ydata, current )
-#            self._xdata = range( len( self._ydata ) ) 
+#            current0 = self.labjack.readRegister( self.AIN0_REGISTER )
+#            current1 = self.labjack.readRegister( self.AIN1_REGISTER )
+
+#            print 'curr0,',current[0]
+#            print 'curr1', current[1]
+#            print current1
+
+
+            self._ydata = np.append( self._ydata, current[0])
+            self._i1data = np.append( self._i1data, current[1] )
+#            self._i2data = np.append( self._i2data, current[2] )
+#            self._i3data = np.append( self._i3data, current[3] )
+
             self._xdata = range( 0, len( self._ydata ))
+            self._tdata = np.append( self._tdata, time.time() - self.start_time)
+#            print 'time', self._tdata
 
-            #Normalize to seconds
-#            self._xdata = self._xdata*self.dt/1000
-
-            print 'ydata', self._ydata
+#            self._ydata = self._ydata
+#            self._i1data = self._i1data
 
     def _set_frequency( self ):
         """ Set the frequency of the shutter, according to a pretedermined plan """
-        d = self.labjack
+        self.labjack
+        
         print 'frequency'
         
+        currTime = time.time() - self.start_time
+        index = int(currTime/self.shutterChunkTime)
+        print 'index', index
+
+        desired_freq = self.shutterPlan[index] #in Hz
+        print 'desired_freq', desired_freq
+
+        pin = 2 #FI02
+        pulsewidth = 50 #PWM %
+
+        if (desired_freq == 0):
+            pulsewidth = 0
+            self._sdata = np.append( self._sdata, 0)            
+        else:
+            divisor = int(self.TIME_CLOCK_BASE/(self.FREQUENCY_CONVERSION_CONSTANT*desired_freq))
+            self.labjack.writeRegister(7002, divisor) #set the divisor
+            actual_freq = self.TIME_CLOCK_BASE/(self.FREQUENCY_CONVERSION_CONSTANT*float(divisor))
+            self._sdata = np.append( self._sdata, actual_freq)
+
+        self.labjack.writeRegister(7200, self.PWM_CONSTANT - (self.PWM_CONSTANT*pulsewidth/100)) #set the duty cycle
 
 
+        ## To test readout of AIN1, 2, 3
+        if (desired_freq == 0):
+            self.labjack.writeRegister(5000, 3.7)
+        else:
+            self.labjack.writeRegister(5000, 0)
+
+        
     def plot_out( self ):
         pl.plot( self.out )
 
@@ -117,12 +187,26 @@ class FiberModel( HasTraits ):
             self.savepath = path
         if name is not None:
             self.filename = name
-#        np.savez( self.savepath + self.outname, self.output )
+
         full_save_path = self.savepath + self.filename
         print full_save_path
+
+        full_save_path_t = full_save_path + '_t'
+        full_save_path_s = full_save_path + '_s'
+        full_save_path_i1= full_save_path + '_i1'
+        full_save_path_i2= full_save_path + '_i2'
+        full_save_path_i3= full_save_path + '_i3'
+
         np.savez( full_save_path, self._ydata )
+        np.savez( full_save_path_t, self._tdata )
+        np.savez( full_save_path_s, self._sdata )
+        np.savez( full_save_path_i1, self._i1data )
+        np.savez( full_save_path_i2, self._i2data )
+        np.savez( full_save_path_i3, self._i3data )
 
 
+
+        self.labjack.close()
 
     def load( self, path = None, name = None ):
         if path is not None:
@@ -169,41 +253,53 @@ class FiberView( HasTraits ):
     def __init__(self, **kwtraits):
         super( FiberView, self ).__init__( **kwtraits )
 
-
-        self.plot_data = ArrayPlotData( x = self.model._xdata, y = self.model._ydata )
+#        self.plot_data = ArrayPlotData( x = self.model._tdata, y = self.model._ydata )
+        self.plot_data = ArrayPlotData( x = self.model._tdata, y = self.model._i1data )
+        
 
         self.plot = Plot( self.plot_data )
         renderer  = self.plot.plot(("x", "y"), type="line", color="green")[0]
 
         # Start the timer!
+
         self.timer = Timer(self.model.dt, self.time_update) # update every 1 ms
 
     def run( self ):
 #        for i in range( 100 ):
+        self.model.start_time = time.time()
+#        while(True):
         self._plot_update()
         self.model._get_current_data()
 
     def time_update( self ):
         """ Callback that gets called on a timer to get the next data point over the labjack """
         if self.recording is True:
+            time1 = time.time()-self.model.start_time
             self.model._set_frequency()
-
-            self.model._get_current_data()
+            time2 = time.time()-self.model.start_time
+            print 'set_freq:', time2-time1
+            self.model._get_current_data()     
+            time3 = time.time()-self.model.start_time
+            print 'get_current_data', time3-time2
             self._plot_update()
+            time4 = time.time()-self.model.start_time
+            print 'plot_update', time4-time3
 
     def _plot_update( self ):
 
-        self.plot_data.set_data( "y", self.model._ydata ) 
-        self.plot_data.set_data( "x", self.model._xdata )
+#        self.plot_data.set_data( "y", self.model._ydata ) 
+        self.plot_data.set_data( "y", self.model._i1data ) 
+        self.plot_data.set_data( "x", self.model._tdata )
         self.plot = Plot( self.plot_data )
         self.plot.plot(("x", "y"), type="line", color="green")[0]
         self.plot.request_redraw()
 
-    # Note: These should be moved to a proper handler
+    # Note: These should be moved to a proper handler (ModelViewController)
 
     def _record_fired( self ):
         self.recording = True
         self.run()
+            
 
     def _stop_fired( self ):
         self.recording = False
@@ -212,11 +308,8 @@ class FiberView( HasTraits ):
         pass
 
     def _save_data_fired( self ):
-#        S = Save( save = Save(), display = TextDisplay() )
-#        S.configure_traits()
-        
-#        self.save( path = S.savepath, name = S.outname )
         self.model.save( path = self.model.savepath, name = self.model.filename )
+        print 'time',self.model._tdata
 
 
     def _save_plot_fired( self ):
@@ -259,7 +352,4 @@ class SaveDialog(HasTraits):
 if __name__ == "__main__":
     F = FiberView()
     F.recording = False
-#    F.run()
-#    1/0
-#    F._get_current_data()
     F.configure_traits()
