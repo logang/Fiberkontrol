Debugged and tested compare_start_and_end_of_epoch(). May need some more testing, but fairly confident it is correct. Also updated get_time_chunks_around_events() to allow for having no normalization subtraction of a baseline value (subtracting the baseline is useful for making pretty perievents, but was getting in the way of compare_start_and_end_of_epoch())

code/analysis/fiber_record_analyze.py
@@ -253,6 +253,8 @@ class FiberAnalyze( object ):
         end = int(self.time_range.split(':')[1])
         end = end if end != -1 else max(self.time_stamps)
         start = start if start != -1 else min(self.time_stamps)
+
+        print "--> Length of time series: ", end-start
         
         # if end == -1:
         #     end = max(FA.time_stamps)
@@ -260,10 +262,10 @@ class FiberAnalyze( object ):
         #     start = min(FA.time_stamps)
 
         #Set a larger resolution to ensure the plotted file size is not too large
-        if end - start <= 100:
+        if end - start <= 100 and end - start > 0:
             resolution = 1
         elif end - start < 500 and end - start > 100:
-            resolution = 1
+            resolution = 10
         elif end - start >= 500 and end - start < 1000:
             resolution = 30
         else:
@@ -299,11 +301,11 @@ class FiberAnalyze( object ):
             ymax = 3.0
             ymin = -1
         elif self.exp_type == 'homecagesocial':
-            ymax = 1.0
-            ymin = -1/3.0
+            ymax = 0.5
+            ymin = -ymax/3.0
         elif self.exp_type == 'homecagenovel':
-            ymax = 1.0
-            ymin = -1/3.0
+            ymax = 0.5
+            ymin = -ymax/3.0
 
         if self.fluor_normalization == 'raw':
             ymax = 10.0
@@ -791,11 +793,12 @@ class FiberAnalyze( object ):
         event_times, with before and after event durations
         specified in window as [before, after] (in seconds).
         Subtracts the baseline value from each chunk (i.e. sets the minimum value in a chunk to 0)
+        Set baseline_window = -1 for no baseline normalization
         """
         window_indices = [self.convert_seconds_to_index( window[0]),
                           self.convert_seconds_to_index( window[1])]
 
-        if baseline_window is not None:
+        if baseline_window is not None and baseline_window != -1:
             baseline_indices = [self.convert_seconds_to_index( baseline_window[0]),
                                 self.convert_seconds_to_index( baseline_window[1])]
 
@@ -805,18 +808,21 @@ class FiberAnalyze( object ):
             e_idx = np.where(e<self.time_stamps)[0][0]
             if (e_idx + window_indices[1] < len(data)-1) and (e_idx - window_indices[0] > 0):
                 chunk = data[range(max(0, (e_idx-window_indices[0])),min(len(data)-1, (e_idx+window_indices[1])))]
-                if baseline_window is not None:
+                if baseline_window is not None and baseline_window != -1:
                     baseline_chunk = data[range(max(0, (e_idx-window_indices[0])), min(len(data)-1, (e_idx+window_indices[1])))]
                     baseline = np.min(baseline_chunk)
+                elif baseline_window == -1:
+                    baseline = 0
                 else:
                     baseline = np.min(chunk)
+
 
                 time_chunks.append(chunk - baseline)
             #except:
              #   print "Unable to extract window:", [(e-window_indices[0]),(e+window_indices[1])]
         return time_chunks
 
-    def plot_perievent_hist( self, event_times, window, out_path=None, plotit=True, subplot=None ):
+    def plot_perievent_hist( self, event_times, window, out_path=None, plotit=True, subplot=None, baseline_window=None ):
         """
         Peri-event time histogram for given event times.
         Plots the time series and their median over a time window around
@@ -836,7 +842,7 @@ class FiberAnalyze( object ):
         print "\t--> Window used for peri-event plot:", window
 
         # get blocks of time series for window around each event time
-        time_chunks = self.get_time_chunks_around_events(self.fluor_data, event_times, window)
+        time_chunks = self.get_time_chunks_around_events(self.fluor_data, event_times, window, baseline_window=baseline_window)
 
         # get time values from frame indices
         window_indices = [ self.convert_seconds_to_index(window[0]),
@@ -1972,7 +1978,7 @@ if __name__ == "__main__":
                       help="Specify path to files with trigger times, minus the '_s.npz' and '_e.npz' suffixes.")
     parser.add_option("-i", "--input-path", dest="input_path",
                       help="Specify the input path.")
-    parser.add_option("", "--time-range", dest="time_range",default=None,
+    parser.add_option("", "--time-range", dest="time_range",default='0:-1',
                       help="Specify a time window over which to analyze the time series in format start:end. -1 chooses the appropriate extremum")
     parser.add_option('-p', "--plot-type", default = '', dest="plot_type",
                       help="Type of plot to produce.")

code/analysis/group_analysis.py
@@ -84,7 +84,7 @@ def group_regression_plot(all_data, options, exp_type='homecagesocial', time_win
 #    pl.ylabel("log length of next interaction")
     pl.show()
 
-def group_bout_heatmaps(all_data, options, exp_type, time_window, df_max=3.5):
+def group_bout_heatmaps(all_data, options, exp_type, time_window, df_max=0.35, event_edge="rising"):
     """
     Save out 'heatmaps' showing time on the x axis, bouts on the y axis, and representing signal
     intensity with color.
@@ -104,37 +104,66 @@ def group_bout_heatmaps(all_data, options, exp_type, time_window, df_max=3.5):
             FA.exp_date = dates
             FA.exp_type = exp_type
             print animal_id, " ", dates, " ", exp_type
-            if(FA.load(file_type="hdf5") != -1):
-                #event_times = FA.get_event_times("rising", int(options.event_spacing))
-                event_times = FA.get_event_times("falling", int(options.event_spacing))
-                print "len(event_times)", len(event_times)
-                time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, event_times, time_window) )
+            if exp_type in animal[dates].keys():
+                if(FA.load(file_type="hdf5") != -1):
+                    event_times = FA.get_event_times(event_edge, int(options.event_spacing))
+                    print "len(event_times)", len(event_times)
+                    time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, event_times, time_window) )
 
-                # Generate a heatmap of activity by bout, with range set between the 5% quantile of
-                # the data and the 'df_max' argument of the function
-                from scipy.stats.mstats import mquantiles
-                baseline = mquantiles( time_arr.flatten(), prob=[0.05])
-                ax.imshow(time_arr, interpolation="nearest",vmin=baseline,vmax=df_max,cmap=pl.cm.afmhot, 
-                            extent=[-time_window[0], time_window[1], 0, time_arr.shape[0]])
-                ax.set_aspect('auto')
-                pl.title("Animal #: "+animal_id+'   Date: '+dates)
-                pl.ylabel('Bout Number')
-                ax.axvline(0,color='white',linewidth=2,linestyle="--")
-                #ax.axvline(np.abs(time_window[0])*time_arr.shape[1]/(time_window[1]-time_window[0]),color='white',linewidth=2,linestyle="--")
+                    # Generate a heatmap of activity by bout, with range set between the 5% quantile of
+                    # the data and the 'df_max' argument of the function
+                    from scipy.stats.mstats import mquantiles
+                    baseline = mquantiles( time_arr.flatten(), prob=[0.05])
+                    ax.imshow(time_arr, interpolation="nearest",vmin=baseline,vmax=df_max,cmap=pl.cm.afmhot, 
+                                extent=[-time_window[0], time_window[1], 0, time_arr.shape[0]])
+                    ax.set_aspect('auto')
+                    pl.title("Animal #: "+animal_id+'   Date: '+dates)
+                    pl.ylabel('Bout Number')
+                    ax.axvline(0,color='white',linewidth=2,linestyle="--")
+                    #ax.axvline(np.abs(time_window[0])*time_arr.shape[1]/(time_window[1]-time_window[0]),color='white',linewidth=2,linestyle="--")
 
-                ax = fig.add_subplot(2,1,2)
-                FA.plot_perievent_hist(event_times, time_window, out_path=None, plotit=True, subplot=ax )
-                pl.ylim([0,df_max])
+                    ax = fig.add_subplot(2,1,2)
+                    FA.plot_perievent_hist(event_times, time_window, out_path=None, plotit=True, subplot=ax )
+                    pl.ylim([0,df_max])
 
-                if options.output_path is not None:
-                    import os
-                    outdir = os.path.join(options.output_path, options.exp_type)
-                    if not os.path.isdir(outdir):
-                        os.makedirs(outdir)
-                    pl.savefig(outdir+'/'+animal_id+'_'+dates+'.png')
-                    print outdir+'/'+animal_id+'_'+dates+'.png'
-                else:
-                    pl.show()
+                    if options.output_path is not None:
+                        import os
+                        outdir = os.path.join(options.output_path, options.exp_type)
+                        if not os.path.isdir(outdir):
+                            os.makedirs(outdir)
+                        pl.savefig(outdir+'/'+animal_id+'_'+dates+'.png')
+                        print outdir+'/'+animal_id+'_'+dates+'.png'
+                    else:
+                        pl.show()
+
+def group_plot_time_series(all_data, options):
+    """
+    Save out time series for each trial, overlaid with 
+    red lines indicating event epochs
+    """
+
+    for animal_id in all_data.keys():
+        # load data from hdf5 file by animal-date-exp_type
+        animal = all_data[animal_id]
+        for date in animal.keys():
+
+            for exp_type in animal[date].keys():
+            
+                FA = FiberAnalyze( options )
+                FA.subject_id = animal_id
+                FA.exp_date = date
+                FA.exp_type = exp_type
+                FA.fluor_normalization = 'deltaF'
+                FA.time_range = '0:-1'
+
+                print animal_id, " ", date, " ", exp_type
+                if(FA.load(file_type="hdf5") != -1):
+                    dir = options.output_path + '/' + FA.exp_type
+                    print dir
+                    if os.path.isdir(dir) is False:
+                        os.makedirs(dir)
+                    FA.plot_basic_tseries(out_path = dir + '/' + FA.subject_id + "_" + FA.exp_date + "_" + FA.exp_type + "_" + 
+                                         str(int(FA.time_range.split(':')[0])) +  "_" + str(int(FA.time_range.split(':')[1])) +"_" ) 
 
 
 def plot_representative_time_series(options, representative_time_series_specs_file):
@@ -190,7 +219,6 @@ def get_novel_social_pairs(all_data, exp1, exp2):
     """
     all_data = an hdf5 file containing all of the time series data
     exp1 and exp2 = the two experiment types to be compared (i.e. homecagesocial and homecagenovel)
-
 
     Returns: a dict where the keys are animal_ids
     and each entry is a dict containing for each exp_type (the keys), 
@@ -248,7 +276,7 @@ def score_of_chunks(ts_arr, metric='area'):
 
 
 
-def compare_start_and_end_of_epoch(all_data, options, time_window=[0,0.25], metric='area', test='ttest'):
+def compare_start_and_end_of_epoch(all_data, options, time_window=[0,0.25], metric='area', test='ttest', plot_perievent=False):
 
     """
     Calculates the difference between the fluorescence in a window at the beginning
@@ -281,28 +309,36 @@ def compare_start_and_end_of_epoch(all_data, options, time_window=[0,0.25], metr
 
                 start_event_times = FA.get_event_times("rising", int(options.event_spacing))
                 end_event_times = FA.get_event_times("falling", int(options.event_spacing))
-                print "len(end_event_times)", len(end_event_times)
-                print "len(start_event_times)", len(start_event_times)
 
                 #--Get an array of time series chunks in a window around each event time
-                start_time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, start_event_times, time_window) )
-                end_time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, end_event_times, time_window) )
+                #reverse_window = [time_window[1], time_window[0]]
+                start_time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, start_event_times, time_window, baseline_window=-1 ) )
+                #before_time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, end_event_times, reverse_window, baseline_window=-1 ))
+                end_time_arr = np.asarray( FA.get_time_chunks_around_events(FA.fluor_data, end_event_times, time_window, baseline_window=-1 ))
 
                 start_scores = np.array(score_of_chunks(start_time_arr, metric))
+                #before_scores = np.array(score_of_chunks(before_time_arr, metric))
                 end_scores = np.array(score_of_chunks(end_time_arr, metric))
                 scores_diff = end_scores - start_scores
+                #scores_diff = end_scores - before_scores
 
                 pair_scores[animal_id][exp_type] = scores_diff
                 pair_avg_scores[animal_id][exp_type] = np.mean(scores_diff)
 
-                # fig = plt.figure()
-                # ax = fig.add_subplot(2,1,1)
-                # title = FA.subject_id + ' ' + FA.exp_date + ' ' + FA.exp_type
-                # ax.set_title(title)
-                # FA.plot_perievent_hist(start_event_times, time_window, out_path=None, plotit=True, subplot=ax )
-                # ax = fig.add_subplot(2,1,2)
-                # FA.plot_perievent_hist(end_event_times, time_window, out_path=None, plotit=True, subplot=ax )
-
+                if(plot_perievent):
+                    fig = plt.figure()
+                    ax = fig.add_subplot(2,1,1)
+                    title = FA.subject_id + ' ' + FA.exp_date + ' ' + FA.exp_type
+                    ax.set_title(title)
+                    FA.plot_perievent_hist(start_event_times, time_window, out_path=options.output_path, plotit=True, subplot=ax, baseline_window=-1  )
+                    #FA.plot_perievent_hist(end_event_times, reverse_window, out_path=options.output_path, plotit=True, subplot=ax, baseline_window=-1 )
+                    ax = fig.add_subplot(2,1,2)
+                    FA.plot_perievent_hist(end_event_times, time_window, out_path=options.output_path, plotit=True, subplot=ax, baseline_window=-1 )
+                    if options.output_path is None:
+                        pl.show()
+                    else:
+                        print "Saving peri-event time series..."
+                        pl.savefig(options.output_path + FA.subject_id + '_' + FA.exp_date + '_' + FA.exp_type + "_perievent_tseries.png")
 
 
     #-- Next, plot all of the pair_scores (vs animal, for now)
@@ -312,20 +348,22 @@ def compare_start_and_end_of_epoch(all_data, options, time_window=[0,0.25], metr
     exp_scores = dict() #key: exp_type, entry: array of avg score for each animal
     for animal_id in pair_avg_scores.keys():
         for exp_type in pair_avg_scores[animal_id].keys():
-
+            print animal_id, exp_type
             if exp_type in exp_scores.keys():
                 exp_scores[exp_type].append(pair_avg_scores[animal_id][exp_type])
             else:
                 exp_scores[exp_type] = [pair_avg_scores[animal_id][exp_type]]
 
     print "Exp_scores", exp_scores
+    plt.close('all')
     plt.figure()
     p1, = plt.plot(exp_scores[exp1], 'o')
     p2, = plt.plot(exp_scores[exp2], 'o')
     plt.legend([p1, p2], [exp1, exp2])
-    plt.xlabel('Mouse')
-    plt.ylabel('Peak value within ' + str(time_window[1]) + 's of end - of start')
-    plt.show()
+    plt.xlabel('Mouse (one mouse per column)')
+    plt.ylabel( 'End - Start (' + metric +' w/in ' + str(time_window[1]) + 's window, avged across epochs)')
+    plt.title('More fluorescence after end than after start of interaction with novel object ')
+    pl.savefig(options.output_path + str(time_window[1]) + '_' + metric+ '_end_minus_start.png')
 
 
     if test == "ttest":
@@ -334,6 +372,10 @@ def compare_start_and_end_of_epoch(all_data, options, time_window=[0,0.25], metr
     if test == "wilcoxon":
             [zstatistic, pvalue] = stats.wilcoxon(exp_scores[exp1], exp_scores[exp2])
             print "normalized area zstatistic: ", zstatistic, " normalized area pvalue: ", pvalue
+
+    plt.show()
+
+
 
 
 
@@ -406,8 +448,9 @@ if __name__ == "__main__":
     time_window = np.array(options.time_window.split(':'), dtype='float32') # [before,after] event in seconds 
 #    group_regression_plot(all_data, options, exp_type=exp_type, time_window=time_window)
 ##    group_bout_heatmaps(all_data, options, exp_type=options.exp_type, time_window=time_window)
+##    group_plot_time_series(all_data, options)
 ##    plot_representative_time_series(options, options.representative_time_series_specs_file)
-    compare_start_and_end_of_epoch(all_data, options, time_window=[0, 0.25], metric='peak', test='ttest')
+    compare_start_and_end_of_epoch(all_data, options, time_window=[0, .1], metric='peak', test='ttest')
 
 
 #----------------------------------------------------------------------------------------   