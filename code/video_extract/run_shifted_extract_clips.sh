df76536d2242d1b299c7d42c9cf947f442b8dffd
Isaac Kauvar <isaackauvar@Isaacs-MacBook-Pro.local>
2013-11-03 11:53:11 -0800

    Added divider clip between different interaction clips.

--- code/video_extract/run_shifted_extract_clips.sh
+++ code/video_extract/run_shifted_extract_clips.sh
@@ -1,6 +1,18 @@
 directory='/Users/isaackauvar/Documents/2012-2013/ZDlab/FiberKontrol/Results/Cell/Plots/Finalz_including_20130920_reaction_time_370ms/print-spike-times'
 
 
+output='clipwindow_1_1'
+clip_window='1:1'
+peak_thresh='0.00'
+divider_clip='black.mp4'
+python extract_clips.py $directory/GC5_NAcprojection/list_of_event_times_GC5_NAcprojection_homecagenovel.pkl -o $output --clip-window=$clip_window --peak-thresh=$peak_thresh --divider-clip=$divider_clip
+if $(false); then
+python extract_clips.py $directory/GC5_NAcprojection/list_of_event_times_GC5_NAcprojection_homecagesocial.pkl -o $output --clip-window=$clip_window --peak-thresh=$peak_thresh  --divider-clip=$divider_clip
+python extract_clips.py $directory/GC5/list_of_event_times_GC5_homecagesocial.pkl -o $output --clip-window=$clip_window --peak-thresh=$peak_thresh  --divider-clip=$divider_clip
+python extract_clips.py $directory/GC5/list_of_event_times_GC5_homecagenovel.pkl -o $output --clip-window=$clip_window --peak-thresh=$peak_thresh  --divider-clip=$divider_clip
+
+
+
 output='clipwindow_0_0'
 clip_window='0:0'
 peak_thresh='0.00'
@@ -9,8 +21,6 @@ python extract_clips.py $directory/GC5_NAcprojection/list_of_event_times_GC5_NAc
 python extract_clips.py $directory/GC5/list_of_event_times_GC5_homecagesocial.pkl -o $output --clip-window=$clip_window --peak-thresh=$peak_thresh
 python extract_clips.py $directory/GC5/list_of_event_times_GC5_homecagenovel.pkl -o $output --clip-window=$clip_window --peak-thresh=$peak_thresh
 
-
-if $(false); then
 
 output='window_0_0_tt'
 clip_window = '0:1'
